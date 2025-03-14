592a7a2ccf0e117ec27a198145f0667305898a00
Antrax <fredcox@gmail.com>
2025-03-14 11:54:51 -0300

    ok

--- main.py
+++ main.py
@@ -2,19 +2,20 @@ import gradio as gr
 from modules.inpainting import create_inpainting_interface
 from modules.flux import create_flux_interface
 
+# Create interfaces for each tab
+inpainting_interface: gr.Blocks = create_inpainting_interface()
+flux_interface: gr.Blocks = create_flux_interface()
 
-def main():
-    # Create interfaces for each tab
-    inpainting_interface = create_inpainting_interface()
-    flux_interface = create_flux_interface()
+# Create tabbed interface (global scope)
+demo = gr.TabbedInterface(
+    interface_list=[inpainting_interface, flux_interface],
+    tab_names=["Inpainting", "Flux"],
+    title=":: FAL-AI Playground ::",
+    theme="d8ahazard/rd_blue",
+)
 
-    # Create tabbed interface
-    demo = gr.TabbedInterface(
-        [inpainting_interface, flux_interface],
-        ["Inpainting", "Flux"],
-        title="FAL-AI Playground",
-    )
 
+def main() -> None:
     # Launch the app
     demo.launch()
 
